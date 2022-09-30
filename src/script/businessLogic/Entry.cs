3ce4a4ec6e9d8d5934683a7ec932e493563987b1
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-09-29 19:20:08 -0500

    Mejorar detección de clientes.

--- src/script/businessLogic/Entry.cs
+++ src/script/businessLogic/Entry.cs
@@ -7,7 +7,7 @@ public class Entry : Node
     {
     #if GODOT_SERVER
         GetTree().ChangeScene("res://src/scene/businessLogic/ServerLobby.tscn");
-    #elif GODOT_32 || GODOT_64
+    #elif GODOT_X11 || GODOT_WINDOWS || GODOT_OSX || GODOT_ANDROID || GODOT_IOS || GODOT_HTML5
         GetTree().ChangeScene("res://src/scene/userInterface/ReversiMenu.tscn");
     #else
         GD.Print("Failed to autodetect platform, defaulting to client.");
