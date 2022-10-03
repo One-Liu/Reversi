5ffc928d2a42ea01909c13de84644b83b33faa30
liu <lesterhero2002@gmail.com>
2022-10-02 19:06:49 -0500

    Validaciones de email y contraseña vacíos. Email elimina espacios vacíos.

--- src/script/userInterface/LogInButton.cs
+++ src/script/userInterface/LogInButton.cs
@@ -15,43 +15,58 @@ public class LogInButton : Button
     
     private void _on_LogInButton_pressed()
     {
-        
         string email = GetParent().GetNode<LineEdit>("EmailLineEdit").Text;
         string password = GetParent().GetNode<LineEdit>("PasswordLineEdit").Text;
-    
-        using (var db = new PlayerContext())
-        {
-            try
-            {
-                var player = db.Player
-                    .SingleOrDefault(b => b.Email == email);
-                
-                byte[] salt = player.Salt;
-                byte[] key = player.Password;
         
-                using (var deriveBytes = new Rfc2898DeriveBytes(password, salt))
+        if(ValidateEmail(email) && ValidatePassword(password)) {
+            email = String.Concat(email.Where(c => !Char.IsWhiteSpace(c)));
+            
+            using (var db = new PlayerContext())
+            {
+                try
                 {
-                    byte[] newKey = deriveBytes.GetBytes(64);
+                    var player = db.Player
+                        .SingleOrDefault(b => b.Email == email);
                     
-                    if(newKey.SequenceEqual(key))
+                    byte[] salt = player.Salt;
+                    byte[] key = player.Password;
+            
+                    using (var deriveBytes = new Rfc2898DeriveBytes(password, salt))
                     {
-                        //GetTree().ChangeScene("res://src/scene/userInterface/MainMenu.tscn");
-                        GD.Print("Log in succesful.");
-                    } 
-                    else
-                    {
-                        GD.Print("Log in failed.");
+                        byte[] newKey = deriveBytes.GetBytes(64);
+                        
+                        if(newKey.SequenceEqual(key))
+                        {
+                            //GetTree().ChangeScene("res://src/scene/userInterface/MainMenu.tscn");
+                            GD.Print("Log in succesful.");
+                        } 
+                        else
+                        {
+                            GD.Print("Log in failed.");
+                        }
                     }
                 }
+                catch(MySqlException e)
+                {
+                    GD.Print(e.Message);
+                }
+                catch(NullReferenceException e)
+                {
+                    GD.Print("Log in failed.");
+                }
             }
-            catch(MySqlException e)
-            {
-                GD.Print(e.Message);
-            }
-            catch(NullReferenceException e)
-            {
-                GD.Print("Log in failed.");
-            }
+        } else {
+            GD.Print("Invalid email or password");
         }
+    
+        
+    }
+    
+    private bool ValidateEmail(String email) {
+        return !String.IsNullOrEmpty(email);
+    }
+    
+    private bool ValidatePassword(String password) {        
+        return !String.IsNullOrEmpty(password);
     }
 }
