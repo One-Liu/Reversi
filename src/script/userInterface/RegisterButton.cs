fde2c9e61c29953ddb0f7c6358828870a5ee9021
Francisco Xavier <franciscoxavieram@gmail.com>
2022-10-03 11:09:23 -0500

    Validación de campos de registrar

--- src/script/userInterface/RegisterButton.cs
+++ src/script/userInterface/RegisterButton.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using MySql.Data.MySqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net.Configuration;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 using ReversiFEI;
 
 public class RegisterButton : Button
@@ -41,6 +48,65 @@ public class RegisterButton : Button
             
             using (var db = new PlayerContext())
             {
+                if( string.IsNullOrEmpty(email))
+                {
+                    GD.Print("Email is empty");
+                }
+                    else
+                    {
+                        if(email == null){
+                             GD.Print("Email is null");
+                        }
+                        if(new EmailAddressAttribute().IsValid(email))
+                           {
+                             GD.Print("Email is valid");
+                        }
+                             else
+                        {
+                             GD.Print("Email is invalid");
+                          }
+                }
+                    
+                if( string.IsNullOrEmpty(username))
+                {
+                    GD.Print("Usmername is empty");
+                }
+                if( string.IsNullOrEmpty(password))
+                {
+                    GD.Print("Password is empty");
+                }
+                else
+                {
+                    if (password.Length < 8 || password.Length > 16)
+                    GD.Print("La contrasenia debe tener entre 8 y 16 caracteres");
+                    else
+                    {
+                        if (!password.Any(char.IsLower) && (!password.Any(char.IsUpper)))	
+                        GD.Print("La contrasenia debe tener al menos una minuscula y una mayuscula");
+                        else
+                        {
+                            if (password.Contains(" "))
+                                GD.Print("La contrasenia no debe tener espacios");
+                            else
+                            {
+                                string specialCh = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+                                char[] specialChArray = specialCh.ToCharArray();
+                                foreach (char ch in specialChArray) 
+                                {
+                                      if (password.Contains(ch))
+                                        playerRegistration.Password = passwordBytes;
+                                    else
+                                        GD.Print("La contrasenia debe tener al menos un caracter especial");
+                                }
+                            }
+                        }
+                    }
+                    
+                if( string.IsNullOrEmpty(confirmPassword))
+                {
+                    GD.Print("TextBox is empty");
+                }
+                
                 try
                 {
                     db.Player.Add(playerRegistration);
@@ -62,4 +128,7 @@ public class RegisterButton : Button
             }
         }
     }
+    
+  }
 }
+
