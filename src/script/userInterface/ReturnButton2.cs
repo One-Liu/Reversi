e83519e081019fc271b611c87fbc5d46e2bc6f3c
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-10-13 20:39:32 -0500

    Añadir interfaz de instrucciones y actualizar archivo de traducción.

---
+++ src/script/userInterface/ReturnButton2.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class ReturnButton2 : Button
+{
+    public override void _Ready()
+    {
+        
+    }
+    
+    private void _on_ReturnButton2_pressed()
+    {
+        GetTree().ChangeScene("res://src/scene/userInterface/MainMenu.tscn");
+    }
+    
+}
