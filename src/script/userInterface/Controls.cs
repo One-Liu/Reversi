c71e6de562fcf002fdde56ec11b8244ffd7b915c
liu <lesterhero2002@gmail.com>
2022-10-21 12:00:03 -0500

    Labels de sign up: campos vacíos; contraseñas no coinciden

--- src/script/userInterface/Controls.cs
+++ src/script/userInterface/Controls.cs
@@ -100,6 +100,7 @@ public class Controls : Node
     
     private void Register()
     {
-        
+        GetNode<RichTextLabel>("../EmptyFields").Visible = true;
+        //GetNode<RichTextLabel>("../DifferentPasswords").Visible = true;
     }
 }
