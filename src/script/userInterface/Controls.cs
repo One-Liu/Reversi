d972422a22bf476ac2f47443eb80bc6d8e5dde8b
liu <lesterhero2002@gmail.com>
2022-10-20 19:21:09 -0500

    Implementación de PopUps, ejemplo de cómo llamarlos

--- src/script/userInterface/Controls.cs
+++ src/script/userInterface/Controls.cs
@@ -1,5 +1,11 @@
 using Godot;
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using MySql.Data.MySqlClient;
+using ReversiFEI;
+using EmailValidation;
 
 public class Controls : Node
 {
@@ -91,7 +97,71 @@ public class Controls : Node
     
     private void LogIn()
     {
-        // Replace with function body.
+        string email = GetParent().GetNode<LineEdit>("EmailLineEdit").Text;
+        string password = GetParent().GetNode<LineEdit>("PasswordLineEdit").Text;
+        
+        if(ValidateEmail(email) && ValidatePassword(password)) {
+            email = String.Concat(email.Where(c => !Char.IsWhiteSpace(c)));
+            
+            using (var db = new PlayerContext())
+            {
+                try
+                {
+                    var player = db.Player
+                        .SingleOrDefault(b => b.Email == email);
+                    
+                    byte[] salt = player.Salt;
+                    byte[] key = player.Password;
+            
+                    using (var deriveBytes = new Rfc2898DeriveBytes(password, salt))
+                    {
+                        byte[] newKey = deriveBytes.GetBytes(64);
+                        
+                        if(newKey.SequenceEqual(key))
+                        {
+                            //GetTree().ChangeScene("res://src/scene/userInterface/MainMenu.tscn");
+                            GD.Print("Log in succesful.");
+                        } 
+                        else
+                        {
+                            GD.Print("Log in failed.");
+                        }
+                    }
+                }
+                catch(MySqlException e)
+                {
+                    AcceptDialog loginFailedPopUp = GetNode<AcceptDialog>("PopUp/ConnectionError");
+                    loginFailedPopUp.Visible = true;
+                    GD.Print(e.Message);
+                }
+                catch(NullReferenceException)
+                {
+                    GD.Print("Log in failed.");
+                }
+            }
+        } else {
+            RichTextLabel invalidEmailOrPassword = GetNode<RichTextLabel>("../InvalidEmailOrPassword");
+            invalidEmailOrPassword.Visible = true;
+            GD.Print("Invalid email or password");
+        }    
+    }
+    
+    private bool ValidateEmail(String email) {
+        var validEmail = true;
+        
+        if(String.IsNullOrEmpty(email))
+        {
+            validEmail = false;
+        }
+        else if(!EmailValidator.Validate(email))
+        {
+            validEmail = false;
+        }
+        return validEmail;
+    }
+    
+    private bool ValidatePassword(String password) {        
+        return !String.IsNullOrEmpty(password);
     }
     
     private void Register()
