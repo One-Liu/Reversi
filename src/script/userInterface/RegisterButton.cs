912dd9bf664158ff541bd4a1ca2705a286527dea
Francisco Xavier <franciscoxavieram@gmail.com>
2022-10-25 10:21:44 -0500

    Registrar

--- src/script/userInterface/RegisterButton.cs
+++ src/script/userInterface/RegisterButton.cs
@@ -17,136 +17,92 @@ public class RegisterButton : Button
 {
     public override void _Ready()
     {
+        
+    }
+    
+    private void _on_RegisterButton_pressed()
+    {
+        
         string email = GetParent().GetNode<LineEdit>("EmailLineEdit").Text;
         string username = GetParent().GetNode<LineEdit>("UsernameLineEdit").Text;
         string password = GetParent().GetNode<LineEdit>("PasswordLineEdit").Text;
         string confirmPassword = GetParent().GetNode<LineEdit>("ConfirmPasswordLineEdit").Text;
         
-        if(ValidateEmail(email) && ValidateUsername(username) && ValidateConfirmPassword(confirmPassword) &&  ValidatePassword(password))
+        if(password.Equals(confirmPassword))
         {
-            email = String.Concat(email.Where(c => !Char.IsWhiteSpace(c)));
-            
-             email = String.Concat(email.Where(c => !Char.IsWhiteSpace(c)));
-            
-            if(password.Equals(confirmPassword))
+            if( string.IsNullOrEmpty(email))
             {
-               
-            byte[] salt;
-            byte[] passwordBytes;
-            
-            using (var deriveBytes = new Rfc2898DeriveBytes(password, 16))
-            {
-                salt = deriveBytes.Salt;
-                passwordBytes = deriveBytes.GetBytes(64);
+                GD.Print("Email is empty");
             }
-            
-            Player playerRegistration = new Player();
-            playerRegistration.Email = email;
-            playerRegistration.Nickname = username;
-            playerRegistration.Password = passwordBytes;
-            playerRegistration.Salt = salt;
-            using (var db = new PlayerContext())
+            else
             {
+                if(new EmailAddressAttribute().IsValid(email))
+                {
+                    GD.Print("Email is valid");
+                }
+                else
+                {
+                    GD.Print("Email is invalid");
+                }
+            }
                 
-                        if(new EmailAddressAttribute().IsValid(email))
-                        {
-                             GD.Print("Email is valid");
-                        }
-                             else
-                        {
-                             GD.Print("Email is invalid");
-                        }
-            bool flag=false;
+            if( string.IsNullOrEmpty(username))
+            {
+                GD.Print("Usmername is empty");
+            }
             if( string.IsNullOrEmpty(password))
             {
                 GD.Print("Password is empty");
-                flag=false;
             }
-            if (password.Length < 8 || password.Length > 16)
+            else
             {
-                GD.Print("Password must have between 8 y 16 characters");
-                flag=false;
-            }
+                if (password.Length < 8 || password.Length > 16)
+                GD.Print("La contrasenia debe tener entre 8 y 16 caracteres");
                 else
                 {
-                    if (!password.Any(char.IsLower) && (!password.Any(char.IsUpper)))
-                    {	
-                        GD.Print("Password must have one lower and one upper letter");
-                        flag=false;
-                    }
+                    if (!password.Any(char.IsLower) && (!password.Any(char.IsUpper)))	
+                    GD.Print("La contrasenia debe tener al menos una minuscula y una mayuscula");
+                    else
+                    {
+                        if (password.Contains(" "))
+                            GD.Print("La contrasenia no debe tener espacios");
                         else
                         {
-                            if (password.Contains(" "))
-                        {
-                            GD.Print("Password must not have spaces");
-                            flag=false;
-                        }
-                            else
+                            string specialCh = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+                            char[] specialChArray = specialCh.ToCharArray();
+                            foreach (char ch in specialChArray) 
                             {
-                                string specialCh = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
-                                char[] specialChArray = specialCh.ToCharArray();
-                                foreach (char ch in specialChArray) 
-                                {
-                                    if(password.Contains(ch))
-                                        playerRegistration.Password = passwordBytes;
-                                        else
-                                        {
-                                            flag=false;
-                                            GD.Print("Password must have at least one special character");
-                                        }
-                                }
+                                    if (!password.Contains(ch))
+                                    //playerRegistration.Password = passwordBytes;
+                                    GD.Print("La contrasenia debe tener al menos un caracter especial");
                             }
                         }
                     }
-                if( string.IsNullOrEmpty(confirmPassword))
-                {
-                    GD.Print("TextBox is empty");
                 }
+                
+            if( string.IsNullOrEmpty(confirmPassword))
+            {
+                GD.Print("TextBox is empty");
             }
-           }
+            
+            try
+            { 
+                if(UserUtilities.SignUp(email, username, password))
+                {
+                    GD.Print("Sign up succesful.");
+                }
+                else
+                {
+                    GD.Print("Sign up failed.");
+                }
+            } 
+            catch (MySqlException e)
+            {
+                GD.Print(e.Message);
+                GD.Print("Sign up failed.");
+            }
         }
     }
-    
-    private void _on_RegisterButton_pressed()
-    {
-       
-    }
-    
-    private bool ValidateUsername(String username)
-    {
-        GD.Print("Invalid email or password");
-        return !String.IsNullOrEmpty(username);    
-    }
-    
-    private bool ValidateEmail(String email) 
-    {
-        var validEmail = true;        
-        if(String.IsNullOrEmpty(email))
-        {
-            GD.Print("Invalid email or password");
-            validEmail = false;
-        }
-        else if(!new EmailAddressAttribute().IsValid(email))
-        {
-            validEmail = false;
-        }
-        return validEmail;
-    }
-    
-    private bool ValidatePassword(String password)
-    {        
-        GD.Print("Invalid email or password");
-        return !String.IsNullOrEmpty(password);
-    }
-    
-    
-    
-    private bool ValidateConfirmPassword(String confirmPassword)
-    {        
-        GD.Print("Invalid email or password");
-        return !String.IsNullOrEmpty(confirmPassword);
-    }
-    
+  }
 }
-
 
