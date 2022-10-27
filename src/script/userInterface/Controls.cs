f12f21d1616de7eaa04708de8cf63e07e6cc9982
liu <lesterhero2002@gmail.com>
2022-10-27 10:59:25 -0500

    Internacionalización y conexiones entre ventanas y popups

--- src/script/userInterface/Controls.cs
+++ src/script/userInterface/Controls.cs
@@ -91,11 +91,142 @@ public class Controls : Node
     
     private void LogIn()
     {
-        GoToMainMenu();
+        ShowNicknameUpdatedPopUp();
+        //GoToMainMenu();
     }
     
     private void Register()
     {
         // Replace with function body.
+    }
+    
+    private void ShowConnectionFailedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/ConnectionFailed").Visible = true;
+    }
+    
+    private void HideConnectionFailedPopUp()
+    {
+        GetNode<WindowDialog>("ConnectionFailed").Visible = false;
+    }
+    
+    private void ShowChallengeDeclinedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/ChallengeDeclined").Visible = true;
+    }
+    
+    private void HideChallengeDeclinedPopUp()
+    {
+        GetNode<WindowDialog>("ChallengeDeclined").Visible = false;
+    }
+    
+    private void ShowFriendAddedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/FriendAdded").Visible = true;
+    }
+    
+    private void HideFriendAddedPopUp()
+    {
+        GetNode<WindowDialog>("FriendAdded").Visible = false;
+    }
+    
+    private void ShowFriendDeletedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/FriendDeleted").Visible = true;
+    }
+    
+    private void HideFriendDeletedPopUp()
+    {
+        GetNode<WindowDialog>("FriendDeleted").Visible = false;
+    }
+    
+    private void ShowSetOfPiecesUpdatedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/SetOfPiecesUpdated").Visible = true;
+    }
+    
+    private void HideSetOfPiecesUpdatedPopUp()
+    {
+        GetNode<WindowDialog>("SetOfPiecesUpdated").Visible = false;
+    }
+    
+    private void ShowPasswordUpdatedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/PasswordUpdated").Visible = true;
+    }
+    
+    private void HidePasswordUpdatedPopUp()
+    {
+        GetNode<WindowDialog>("PasswordUpdated").Visible = false;
+    }
+    
+    private void ShowNicknameUpdatedPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/NicknameUpdated").Visible = true;
+    }
+    
+    private void HideNicknameUpdatedPopUp()
+    {
+        GetNode<WindowDialog>("NicknameUpdated").Visible = false;
+    }
+    
+    private void ShowNewChallengePopUp()
+    {
+        GetNode<WindowDialog>("PopUp/NewChallenge").Visible = true;
+    }
+    
+    private void HideNewChallengePopUp()
+    {
+        GetNode<WindowDialog>("NewChallenge").Visible = false;
+    }
+    
+    private void ShowChangeNicknamePopUp()
+    {
+        GetNode<WindowDialog>("PopUp/ChangeNickname").Visible = true;
+    }
+    
+    private void HideChangeNicknamePopUp()
+    {
+        GetNode<WindowDialog>("ChangeNickname").Visible = false;
+    }
+    
+    private void ShowChangePasswordPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/ChangePassword").Visible = true;
+    }
+    
+    private void HideChangePasswordPopUp()
+    {
+        GetNode<WindowDialog>("ChangePassword").Visible = false;
+    }
+    
+    private void ShowChangeSetOfPiecesPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/ChangeSetOfPieces").Visible = true;
+    }
+    
+    private void HideChangeSetOfPiecesPopUp()
+    {
+        GetNode<WindowDialog>("ChangeSetOfPieces").Visible = false;
+    }
+    
+    private void ShowChangeAvatarPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/ChangeAvatar").Visible = true;
+    }
+    
+    private void HideChangeAvatarPopUp()
+    {
+        GetNode<WindowDialog>("ChangeAvatar").Visible = false;
+    }
+    
+    private void ShowPlayerOptionsPopUp()
+    {
+        GetNode<WindowDialog>("PopUp/PlayerOptions").Visible = true;
+    }
+    
+    private void HidePlayerOptionsPopUp()
+    {
+        GetNode<WindowDialog>("PlayerOptions").Visible = false;
     }
 }
