1cce60bd8aa1cb6a40f3620472ca1b3374089312
One-Liu <lesterhero2002@gmail.com>
2022-12-04 13:33:02 -0600

    Plantilla networkutilities tests

---
+++ tests/NetworkUtilitiesTests.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using ReversiFEI.Network;
+
+namespace Tests.Reversi
+{
+    [Title("UserUtilities tests")]
+    
+    [Pre(nameof(RunBeforeTestMethod))]
+    
+    public class NetworkUtilitiesTests : WAT.Test
+    {
+        public void RunBeforeTestMethod()
+        {
+            
+        }
+    }
+}
